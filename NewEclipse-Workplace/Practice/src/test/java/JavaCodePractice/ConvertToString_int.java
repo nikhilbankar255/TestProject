44e38429ada186489b21b63f53da34016a77badc
Nikhil <nikhilbankar3839@gmail.com>
2026-08-04 14:20:46 +0530

    "convert number to String and to Integer"

---
+++ NewEclipse-Workplace/Practice/src/test/java/JavaCodePractice/ConvertToString_int.java
@@ -0,0 +1,18 @@
+package JavaCodePractice;
+
+public class ConvertToString_int {
+
+	public static void main(String[] args) {
+
+		int i=6;
+		String s= String.valueOf(i);
+		System.out.println("To String:"+s);
+	
+		
+		int ii= Integer.parseInt(s);
+		System.out.println("To int:"+ii);
+
+		
+	}
+
+}
